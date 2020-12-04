478e2f9bc9d62a8d0f0f58df53037b0ffd927462
Alexander Gordeyko <axgord@gmail.com>
2020-12-04 20:28:45 +0300

    waittime

--- src/models/Queue.hx
+++ src/models/Queue.hx
@@ -1,5 +1,8 @@
 package models;
 
+import pony.text.tpl.TplData.TplShortTag;
+import pony.text.tpl.ITplPut;
+import pony.net.http.modules.mmodels.ModelPut;
 import pony.db.DBV;
 import pony.net.http.modules.mmodels.Model;
 import pony.net.http.modules.mmodels.ModelConnect;
@@ -43,7 +46,16 @@ import pony.tests.Errors;
 @:build(com.dongxiguo.continuation.Continuation.cpsByMeta(':async'))
 @:keep class QueueConnect extends ModelConnect {
 
-	@:async function many(): Array<Dynamic> return @await db.select(ID, NAME, REG).where((begin == null) & end == null).asc(REG).get();
+	@:async function many(): Array<Dynamic> {
+		var s: Int = @await rawWaitTime();
+		var r = @await db.select(ID, NAME, REG).where((begin == null) & end == null).asc(REG).get();
+		return r.map(e -> ({
+			id: e.id,
+			name: e.name,
+			reg: e.reg + s
+		}));
+
+	}
 	@:async function insert(name: String): Bool return @await db.insert([NAME => (name: DBV), REG => DBV.TIMESTAMP]);
 	@:async function delete(id: Int): Bool return @await db.where(id == $id).delete();
 	@action('Delete') @:async function start(id: Int): Bool return @await db.where(id == $id).update([BEGIN => DBV.TIMESTAMP]);
@@ -58,6 +70,40 @@ import pony.tests.Errors;
 		e.test(name.length < Queue.NAME_MIN, SHORT);
 		e.test(name.length > Queue.NAME_MAX, LONG);
 		return e;
+	}
+
+	override public function tpl(parent: ITplPut): ITplPut {
+		return new QueuePut(this, null, parent);
+	}
+
+	@:async
+	public function rawWaitTime(): Int {
+		var r = @await db.select(BEGIN, END).where((begin != null) & end != null).get();
+		var s: Int = 0;
+		for (e in r) s += Std.int(e.end) - Std.int(e.begin);
+		return s;
+	}
+
+	@:async
+	public function waitTime(): String {
+		var s: Int = @await rawWaitTime();
+		return '${Std.int(s / 60)} мин ${s % 60} сек';
+	}
+
+
+}
+
+@:build(com.dongxiguo.continuation.Continuation.cpsByMeta(":async"))
+class QueuePut extends ModelPut {
+
+	@:async @:puper
+	override public function shortTag(name:String, arg:String, ?kid:ITplPut):String {
+		if (name == 'waittime')
+			return @await cast(a, QueueConnect).waitTime();
+		if (parent == null)
+			return '%' + name + '%';
+		else
+			return @await super.shortTag(name, arg, kid);
 	}
 
 }
